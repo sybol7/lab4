7ae9cc9dc2b3bb2885c3f9e795a65d0210e434fb
Sylvain Bolduc <sylvain.bolduc.1@ulaval.ca>
2019-10-19 10:12:49 -0400

    Ajustement du code pour mon lab4

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>To Do list</h1>
+        <button class="btn btn-primary" id="user_id_btn" @click="submit">Get user id</button>
+        <hr>
+        <p>User id : {{ user_id }}</p>
+        <br><br>
+        <div class="form-group">
+          <label>Task Name</label>
+          <input type="text" class="form-control" v-model="taskname">
+        </div>
+        <button class="btn btn-primary" @click="sendToDo">Add To Do</button>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="t in tasks" :key="t.item" :id=t.id>
+              <button type="button" class="btn btn-danger btn-sm">delete</button>
+              <input type="text" class="form-control" :placeholder=t.name>
+              <button type="button" class="btn btn-info btn-sm">Modify</button>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      user_id: '',
+      taskname: '',
+      tasks: []
+    }
+  },
+  methods: {
+    submit() {
+      this.$http.post("https://glo3102lab4.herokuapp.com/users")
+              .then(response => {
+                return response.json();
+              }, error => {
+                // eslint-disable-next-line no-console
+                console.log(error);
+                // eslint-disable-next-line no-console
+              }).then(data => {
+        this.user_id = data['id'];
+        document.getElementById('user_id_btn').disabled = true;
+      })
+    },
+    sendToDo () {
+
+      this.$http.post('https://glo3102lab4.herokuapp.com/' + this.user_id + '/tasks', {"name":this.taskname})
+              .then(response => {
+                return response.json();
+              }).then( task => {
+                this.tasks.push(task);
+        // eslint-disable-next-line no-console
+        console.log(this.tasks);
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
